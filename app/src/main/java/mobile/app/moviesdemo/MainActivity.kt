c1411859daf7d90dc0461a1d8fe4719edc6ad4a7
bedirhan <bedirhan.bedir@hotmail.com>
2025-03-03 23:54:10 +0300

    Complete MainActivity ui design

--- app/src/main/java/mobile/app/moviesdemo/MainActivity.kt
+++ app/src/main/java/mobile/app/moviesdemo/MainActivity.kt
@@ -10,6 +10,8 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -20,18 +22,24 @@ import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -48,8 +56,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MoviesDemoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    MovieScreen(viewModel)
+                Scaffold(
+                    topBar = { MovieToolbar() },
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+                    MovieScreen(viewModel,innerPadding)
 
                 }
             }
@@ -58,15 +69,16 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MovieScreen(viewModel: MoviesViewModel) {
+fun MovieScreen(viewModel: MoviesViewModel,paddingValues: PaddingValues) {
     val moviesState by viewModel.moviesState.collectAsState()
 
     LazyColumn(
         modifier = Modifier
             .fillMaxSize()
             .background(Color.Black)
-            .padding(8.dp)
+            .padding(paddingValues)
     ) {
+
         items(moviesState.list) { movie -> // Doğrudan listeyi kullanıyoruz
             SectionTitle(movie.title)
             MovieRow(movie.movieList)
@@ -131,5 +143,27 @@ fun MovieCard(imageUrl: String?) {
                 Text(text = "NO PHOTO AVAILABLE", color = Color.White, fontSize = 12.sp)
             }
         }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MovieToolbar() {
+    Column {
+        TopAppBar(
+            title = {
+                Text(
+                    text = "Movies Demo",
+                    color = Color.White,
+                    modifier = Modifier.fillMaxWidth(),
+                    textAlign = TextAlign.Center
+                )
+            },
+            colors = TopAppBarDefaults.mediumTopAppBarColors(
+                containerColor = Color.Black
+            )
+        )
+
+        Divider(color = Color.Red, thickness = 2.dp)
     }
 }
